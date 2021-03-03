818d29b241620ff0f56a20ae244c416a49e53fe6
AshveerB <ashveerbhayroo@gmail.com>
2021-03-03 08:29:34 -0800

    basic client routing and links

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <div>
-            Navigation
-        </div>
-    );
+			<div>
+				<div>Brand</div>
+				<nav>
+					<Link to='/home'>Home</Link>
+					<Link to='/signin'>Sign In</Link>
+				</nav>
+			</div>
+		);
 };
 
 export default Navigation;
--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 const SignIn = () => {
-    return (
-        <div>
-            Sign In
-        </div>
-    );
+	return (
+		<div>
+			Sign In
+			<div>
+				Not Signed Up Yet? <Link to='/signup'>Register</Link>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
