a1f56f64b86b0ae892f40da84ffa394e70e8936b
Chris L <c.aventine@gmail.com>
2021-03-05 13:10:26 -0600

    Pull in images

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
+
 
 const Home = () => {
     return (
         <div>
-            <img src="" />
-            <br></br>
+            <img id="green" src="Helpaneighbor.png" />
+            <h3>Help a Neighbor Save their Home</h3>
             <Link to='/Map'>Find Listing</Link>
             <br></br>
-            <img src="" />
-            <h2>Need help with your mortgage payment?</h2>
+            <img id="green" src="Needhelp.png" />
+            <h3>Need help with your mortgage payment?</h3>
             <Link to='/signup'>Create Listing</Link>
-
+            <br></br>
+            <img id="green" src="Home.png" />
 
 
         </div>
