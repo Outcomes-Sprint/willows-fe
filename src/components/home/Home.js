c048e645f8aa8caa78f7180ef806784ca9099049
Chris L <c.aventine@gmail.com>
2021-03-05 16:04:58 -0600

    Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Home.css';
 
 
 const Home = () => {
     return (
         <div>
-            <img className="green" src="Helpaneighbor.png" />
+            <img className="main" src="Helpaneighbor.png" />
             <h3>Help a Neighbor Save their Home</h3>
-            <Link to='/Map'>Find Listing</Link>
+            <button><NavLink className="link" exact to='/Map'>Find Listing</NavLink></button>
             <br></br>
-            <img className="green" src="Needhelp.png" />
+            <img className="main" src="Needhelp.png" />
             <h3>Need help with your mortgage payment?</h3>
-            <Link to='/signup'>Create Listing</Link>
+            <button><NavLink className="link" exact to='/signup'>Create Listing</NavLink></button>
             <br></br>
-            <img className="green" src="Home.png" />
+            <br></br>
+            <img className="main" src="Home.png" />
 
 
         </div>
