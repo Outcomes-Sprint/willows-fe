20640710392879b68c3178da08d4ef992ff9ac43
AshveerB <ashveerbhayroo@gmail.com>
2021-03-07 12:02:21 -0800

    screen structure

---
+++ src/components/willows/Mortgage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mortgage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Mortgage;
---
+++ src/components/willows/SetUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SetUp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SetUp;
