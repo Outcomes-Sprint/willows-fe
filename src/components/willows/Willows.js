f73abe2efbbce5e8bee77d96424434be210ec333
AshveerB <ashveerbhayroo@gmail.com>
2021-03-07 12:36:41 -0800

    mortgage info page set up

--- src/components/willows/Willows.js
+++ src/components/willows/Willows.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SetUp from './SetUp'
+import Mortgage from './Mortgage'
 
 const Willows = () => {
     return (
         <div>
             <SetUp />
+            <Mortgage />
         </div>
     );
 };
