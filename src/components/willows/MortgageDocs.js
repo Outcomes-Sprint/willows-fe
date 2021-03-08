ba7c73f3b56d5c866288bc3541f2f4ca680e5e52
AshveerB <ashveerbhayroo@gmail.com>
2021-03-07 16:25:28 -0800

    willow doc upload functionality

--- src/components/willows/MortgageDocs.js
+++ src/components/willows/MortgageDocs.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 import UploadsImg from './images/uploadsimg.png'
 
 const MortgageDocs = () => {
 	const [fileNames, setFileNames] = useState([]);
 	const handleDrop = (acceptedFiles) =>
 		setFileNames(acceptedFiles.map((file) => file.name));
+	const handleClickSend = (event) => {
+		event.preventDefault();
+		Axios({
+			url: '',
+			method: 'POST',
+			data: acceptedFiles,
+		});
+	};	
 	return (
 		<div className='dropBoxContainer'>
 			<Dropzone onDrop={handleDrop}>
@@ -13,10 +22,13 @@ const MortgageDocs = () => {
 					<div {...getRootProps({ className: 'dropzone' })}>
 						<input {...getInputProps()} />
 						<p>Upload</p>
-                        <img src={UploadsImg} />
+						<img src={UploadsImg} />
 					</div>
 				)}
-			</Dropzone><br /><br /><br />
+			</Dropzone>
+			<br />
+			<br />
+			<br />
 			<div>
 				<strong>Files:</strong>
 				<ul>
@@ -25,9 +37,12 @@ const MortgageDocs = () => {
 					))}
 				</ul>
 			</div>
-            <div>Upload Pre-forclosure Notice</div>
-            <div>We can use this document to verify your listing, so that you can get the help you need.</div>
-            <button>Confirm</button>
+			<div>Upload Pre-forclosure Notice</div>
+			<div>
+				We can use this document to verify your listing, so that you can get the
+				help you need.
+			</div>
+			<button onClick={handleClickSend}>Confirm</button>
 		</div>
 	);
 };
