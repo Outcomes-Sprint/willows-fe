ce27a659c9e564c71c8c864288cf65680732be75
AshveerB <ashveerbhayroo@gmail.com>
2021-03-07 16:21:25 -0800

    willow payment info functionality

--- src/components/willows/Payment.js
+++ src/components/willows/Payment.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Axios from 'axios';
 
 const Payment = () => {
+    const initialState = {
+		bankAccountNumber: '',
+		accountRouting: '',
+		moneyNeeded: '',
+	};
+	const [formState, setFormState] = useState(initialState);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		Axios({
+			url: '',
+			method: 'POST',
+			data: formState,
+		}).then((res) => {
+
+		});
+		setFormState(initialState);
+	};
+	const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.id]: event.target.value });
+	};
     return (
 			<div>
 				<div>Provide your payment informoation to recieve donations</div>
-				<form>
-					<label htmlFor='bankAccountNumber'>What's your bank account number?</label>
-					<input 
-                        id='bankAccountNumber'
-                        placeholder='Bank account number'
-                    />
+				<form onSubmit={handleSubmit}>
+					<label htmlFor='bankAccountNumber'>
+						What's your bank account number?
+					</label>
+					<input
+						id='bankAccountNumber'
+						onChange={handleChange}
+						value={formState.bankAccountNumber}
+						placeholder='Bank account number'
+					/>
 					<label htmlFor='accountRouting'>What's your routing number?</label>
-					<input 
-                        id='accountRouting'
-                        placeholder='Routing number'
-                    />
+					<input
+						id='accountRouting'
+						onChange={handleChange}
+						value={formState.accountRouting}
+						placeholder='Routing number'
+					/>
 					<label htmlFor='moneyNeeded'>How much money will you need?</label>
-					<input 
-                        id='moneyNeeded'
-                        placeholder='For example: $4,000'
-                    />
-                    <div>Link Paypal</div>
-                    <div>P</div>
-                    <div>Great! Next, We'll need: </div>
-                    <button type='submit'>Documentation</button>
+					<input
+						id='moneyNeeded'
+						onChange={handleChange}
+						value={formState.moneyNeeded}
+						placeholder='For example: $4,000'
+					/>
+					<div>Link Paypal</div>
+					<div>P</div>
+					<div>Great! Next, We'll need: </div>
+					<button type='submit'>Documentation</button>
 				</form>
 			</div>
 		);
