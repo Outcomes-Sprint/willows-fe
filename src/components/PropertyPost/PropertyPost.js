ab785f82a5ae7ef3d1257dd81eb2cc9943412a54
atlfinesse <finesse.cap@gmail.com>
2021-03-08 21:46:03 -0800

    Added post success message

--- src/components/PropertyPost/PropertyPost.js
+++ src/components/PropertyPost/PropertyPost.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './PropertyPost.css';
-import Axios from 'axios'
+import Axios from 'axios';
 import { APIURL } from '../../config';
-
+import ThankYou from '../../media/images/ty.png';
 
 function PropertyPost({ setRenderComponent }) {
+	const [submitted, setSubmitted] = useState(false);
+
 	const [propertyForm, setPropertyForm] = useState({
 		address: '',
 		owner: '',
@@ -28,17 +30,22 @@ function PropertyPost({ setRenderComponent }) {
 		phase_6: 'Eviction',
 	};
 
-        const handleSubmit = (e) => {
-			e.preventDefault();
-			Axios({
-				method: 'post',
-				url: APIURL,
-				headers: {
-					// Authorization: `Token ${token}`,
-				},
-				data: propertyForm,
-			}).then(setRenderComponent('Show'));
-		};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		Axios({
+			method: 'post',
+			url: APIURL,
+			headers: {
+				// Authorization: `Token ${token}`,
+			},
+			data: propertyForm,
+		}).then(setSubmitted(true));
+		console.log(propertyForm);
+
+		setTimeout(() => {
+			setRenderComponent('Show');
+		}, 2500);
+	};
 
 	const handleChange = (e) => {
 		e.preventDefault();
@@ -51,23 +58,129 @@ function PropertyPost({ setRenderComponent }) {
 	return (
 		<div>
 			<h1>PropertyPost</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor='monthlyPayment'>
-						What's your monthly mortgage payment?
-					</label>
-					<br />
-					<input
-						type='text'
-						name='address'
-						placeholder='Enter Address'
-						value={propertyForm.address}
-						onChange={handleChange}
-					/>
-				</div>
 
-                <button>Submit</button>
-			</form>
+			{submitted ? (
+				<>
+					<div>Thank you!</div>
+					<div><img src={ThankYou} alt='Thank You' /></div>
+					<div>
+						We'll send you an email once your listing is approved.
+					</div>
+					<div>(It should bee within a week)</div>
+				</>
+			) : (
+				<>
+					<div>Tell us a bit about your mortgage</div>
+					<form onSubmit={handleSubmit}>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<label htmlFor='monthlyPayment'>
+								What's your monthly mortgage payment?
+							</label>
+							<br />
+							<input
+								type='text'
+								name='address'
+								placeholder='Enter Address'
+								value={propertyForm.address}
+								onChange={handleChange}
+							/>
+						</div>
+
+						<button>Submit</button>
+					</form>
+				</>
+			)}
 		</div>
 	);
 }
--- src/pages/Property/Property.js
+++ src/pages/Property/Property.js
@@ -16,6 +16,7 @@ function Property() {
 			) : (
 				<PropertyPost setRenderComponent={setRenderComponent} />
 			)}
+
 		</div>
 	);
 }
