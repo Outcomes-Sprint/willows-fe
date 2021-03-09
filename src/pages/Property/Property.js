15cea038944880761b0f048a49b839d3b9b34759
atlfinesse <finesse.cap@gmail.com>
2021-03-08 20:58:45 -0800

    Created initial property form state

--- src/pages/Property/Property.js
+++ src/pages/Property/Property.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Property.css';
 
 function Property() {
+    const [propertyForm, setPropertyForm] = useState({
+        address: '',
+        owner: '',
+        user: '',
+        story: '',
+        amount_needed: '',
+        monthly_payment: '',
+        missed_payments: '',
+        approved: '',
+        documentation: '',
+        foreclosure_status: ''
+    })
+
+    const foreclosureStatusOptions = {
+        pre_foreclosure: 'Pre-Foreclosure',
+        phase_1: 'Payment Default',
+        phase_2: 'Notice of Default',
+        phase_3: 'Notice of Trustee\'s Sale',
+        phase_4: 'Trustee\'s Sale',
+        phase_5: 'Real Estate Owned(REO)',
+        phase_6: 'Eviction',
+    }
+
     return (
         <div>
             
