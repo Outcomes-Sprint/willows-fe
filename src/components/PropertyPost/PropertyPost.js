3d468724499c95b80921b61399ef739dfa24ce35
atlfinesse <finesse.cap@gmail.com>
2021-03-10 14:18:02 -0800

    Changed PropertyPost to have sections

--- src/components/PropertyPost/PropertyPost.js
+++ src/components/PropertyPost/PropertyPost.js
@@ -6,6 +6,7 @@ import ThankYou from '../../media/images/ty.png';
 
 function PropertyPost({ setRenderComponent, propertyData }) {
 	const [submitted, setSubmitted] = useState(false);
+	const [formSection, setFormSection] = useState(1)
 
 	const [propertyForm, setPropertyForm] = useState({
 		address: '',
@@ -68,122 +69,157 @@ function PropertyPost({ setRenderComponent, propertyData }) {
 				<>
 					<div>Tell us a bit about your mortgage</div>
 					<form onSubmit={handleSubmit}>
-						<div>
-							<label htmlFor='address'>
-								What is the address of the property?
-							</label>
-							<br />
-							<input
-								type='text'
-								name='address'
-								placeholder='123 Uh Oh Lane'
-								value={propertyForm.address}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>
-							<label htmlFor='owner'>
-								What is the owner's name?
-							</label>
-							<br />
-							<input
-								type='text'
-								name='owner'
-								placeholder='John Appleseed'
-								value={propertyForm.owner}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>
-							<label htmlFor='monthly_payment'>
-								What's your monthly mortgage payment?
-							</label>
-							<br />
-							<input
-								type='number'
-								name='monthly_payment'
-								placeholder='$1200'
-								value={propertyForm.monthly_payment}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>
-							<label htmlFor='amount_needed'>
-								What is the total amount that you need donated?
-							</label>
-							<br />
-							<input
-								type='number'
-								name='amount_needed'
-								placeholder='$50,000'
-								value={propertyForm.amount_needed}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>
-							<label htmlFor='missed_payments'>
-								How many payments have you missed?
-							</label>
-							<br />
-							<input
-								type='number'
-								name='missed_payments'
-								placeholder='4'
-								value={propertyForm.missed_payments}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>
-							<label htmlFor='foreclosure_status'>
-								What stage of the foreclosure process are you
-								in?
-							</label>
-							<br />
-							<select
-								name='foreclosure_status'
-								onChange={handleChange}>
-								<option value={propertyForm.foreclosure_status}>
-									Pre-Foreclosure
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Payment Default
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Notice of Default
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Notice of Trustee's Sale
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Trustee's Sale
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Real Estate Owned(REO)
-								</option>
-								<option value={propertyForm.foreclosure_status}>
-									Eviction
-								</option>
-							</select>
-						</div>
-						<div>
-							<label htmlFor='story'>
-								What is the story behind your foreclosure
-								situation?
-							</label>
-							<br />
-							<input
-								type='text'
-								name='story'
-								placeholder='I messed up like real bad'
-								value={propertyForm.story}
-								onChange={handleChange}
-							/>
-						</div>
-						<div>Upload Pre-forclosure Notice</div>
-						<div>
-							We can use this document to verify your listing, so
-							that you can get the help you need.
-						</div>
+						{formSection === 1 ? (
+							<>
+								<div>
+									<label htmlFor='address'>
+										What is the address of the property?
+									</label>
+									<br />
+									<input
+										type='text'
+										name='address'
+										placeholder='123 Uh Oh Lane'
+										value={propertyForm.address}
+										onChange={handleChange}
+									/>
+								</div>
+								<div>
+									<label htmlFor='owner'>
+										What is the owner's name?
+									</label>
+									<br />
+									<input
+										type='text'
+										name='owner'
+										placeholder='John Appleseed'
+										value={propertyForm.owner}
+										onChange={handleChange}
+									/>
+								</div>
+							</>
+						) : null}
+						{formSection === 2 ? (
+							<>
+								<div>
+									<label htmlFor='monthly_payment'>
+										What's your monthly mortgage payment?
+									</label>
+									<br />
+									<input
+										type='number'
+										name='monthly_payment'
+										placeholder='$1200'
+										value={propertyForm.monthly_payment}
+										onChange={handleChange}
+									/>
+								</div>
+								<div>
+									<label htmlFor='amount_needed'>
+										What is the total amount that you need
+										donated?
+									</label>
+									<br />
+									<input
+										type='number'
+										name='amount_needed'
+										placeholder='$50,000'
+										value={propertyForm.amount_needed}
+										onChange={handleChange}
+									/>
+								</div>
+								<div>
+									<label htmlFor='missed_payments'>
+										How many payments have you missed?
+									</label>
+									<br />
+									<input
+										type='number'
+										name='missed_payments'
+										placeholder='4'
+										value={propertyForm.missed_payments}
+										onChange={handleChange}
+									/>
+								</div>
+							</>
+						) : null}
+						{formSection === 3 ? (
+							<>
+								<div>
+									<label htmlFor='foreclosure_status'>
+										What stage of the foreclosure process
+										are you in?
+									</label>
+									<br />
+									<select
+										name='foreclosure_status'
+										onChange={handleChange}>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Pre-Foreclosure
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Payment Default
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Notice of Default
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Notice of Trustee's Sale
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Trustee's Sale
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Real Estate Owned(REO)
+										</option>
+										<option
+											value={
+												propertyForm.foreclosure_status
+											}>
+											Eviction
+										</option>
+									</select>
+								</div>
+								<div>
+									<label htmlFor='story'>
+										What is the story behind your
+										foreclosure situation?
+									</label>
+									<br />
+									<input
+										type='text'
+										name='story'
+										placeholder='I messed up like real bad'
+										value={propertyForm.story}
+										onChange={handleChange}
+									/>
+								</div>
+								<div>Upload Pre-forclosure Notice</div>
+								<div>
+									We can use this document to verify your
+									listing, so that you can get the help you
+									need.
+								</div>
+							</>
+						) : null}
 
 						<button>Submit</button>
 					</form>
