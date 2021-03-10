aaa4b6320fb32e5a0f25b29216e1b92c91dfeaa1
atlfinesse <finesse.cap@gmail.com>
2021-03-10 14:38:38 -0800

    Created donation confirmation page

---
+++ src/components/DonationConfirmation/DonationConfirmation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SweetHome from '../../media/images/sweet-home.svg'
+
+function DonationConfirmation() {
+    return (
+        <div>
+            thank you
+        </div>
+    )
+}
+
+export default DonationConfirmation
