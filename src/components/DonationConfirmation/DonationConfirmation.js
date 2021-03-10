28ea9583e93ed327a309ed9e58912821f5ec1965
atlfinesse <finesse.cap@gmail.com>
2021-03-10 14:43:12 -0800

    Finished donation thank you page

--- src/components/DonationConfirmation/DonationConfirmation.js
+++ src/components/DonationConfirmation/DonationConfirmation.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import SweetHome from '../../media/images/sweet-home.svg'
 
 function DonationConfirmation() {
+	const [redirect, setRedirect] = useState(false);
+
+    console.log(redirect)
+
+    if (redirect) {
+        return <Redirect to='/home' />;
+    }
+
     return (
         <div>
-            thank you
+            <h1>Thank you for your donation!</h1>
+            <br/>
+            <img src={SweetHome} alt='thank you'/>
+            <br/>
+            <br/>
+            <button onClick={() => setRedirect(true)}>Return to Home</button>
         </div>
     )
 }
